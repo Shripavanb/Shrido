f8cd6b3fdb01a02ff85139153340752e5f00c058
ShriGit01 <shripavanb@gmail.com>
2025-08-09 17:49:45 +0530

    First Sucessful List view display

--- app/src/main/java/in/shrido/shrido/MainActivity.kt
+++ app/src/main/java/in/shrido/shrido/MainActivity.kt
@@ -1,23 +1,38 @@
 package `in`.shrido.shrido
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.EditText
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+val listView: ListView = findViewById(R.id.main_list)
+        val listItems = arrayListOf("Hyderabad",
+            "Armoor",
+            "Nizamabad",
+            "Kamareddy")
+
+        val listAdapter= ArrayAdapter(this, android.R.layout.simple_list_item_1,listItems)
+        listView.adapter = listAdapter
     }
 
     fun Onclick_Search(view: View) {
@@ -26,4 +41,11 @@ class MainActivity : AppCompatActivity() {
 
         //val myData = RideData.RideDetails( sourcetext.toString(),desttext.toString())
     }
+
+     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.mymenu)
+        return super.onCreateOptionsMenu(menu)
+    }
 }
+
+private fun MenuInflater.inflate(menuRes: Int) {}

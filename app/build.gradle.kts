ef25fad0234d3e1ce9f486bbb6961a97256f2d0b
ShriGit01 <shripavanb@gmail.com>
2025-08-23 12:34:44 +0530

    Bottom Nav Bar is Working Sucessfully , but dont push to main yet

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation("com.google.code.gson:gson:2.13.1")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.fragment:fragment-ktx:1.8.9")
     implementation(libs.identity.jvm)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/in/shrido/shrido/MainActivity.kt
+++ app/src/main/java/in/shrido/shrido/MainActivity.kt
@@ -1,27 +1,30 @@
 package `in`.shrido.shrido
 
+
 import android.icu.text.SimpleDateFormat
 import android.os.Bundle
-import android.text.format.DateFormat
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
 import android.widget.EditText
+
 import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.android.identity.cbor.DataItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.Date
 import java.util.SimpleTimeZone
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import `in`.shrido.shrido.databinding.ActivityMainBinding
 import java.io.IOException
 
 
 class MainActivity : AppCompatActivity() {
-
 
     private val gutceurrentDate: String = "12/08/2025"
     private lateinit var listView: ListView
@@ -29,55 +32,96 @@ class MainActivity : AppCompatActivity() {
     private val itemList = mutableListOf<RideData>()
     private val itemList2 = mutableListOf<RideData>()
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
 
-        val listView: ListView = findViewById(R.id.main_list)
-        //listView = findViewById(R.id.main_list) // Assuming you have a ListView in activity_main.xml
+        // Set initial fragment
+        if (savedInstanceState == null) {
+            replaceFragment(Home())
+        }
 
-        // Date Format
-        val sdf = SimpleDateFormat("dd/M/yyyy")
-        val currentDate = sdf.format(Date())
-        System.out.println(" Todays DATE is  "+currentDate)
-
-        //time Format
-
-        // Initialize with some data
-        itemList.add(RideData( currentDate,"Hyd", "Armor","5Pm","Kamareddy"))
-        itemList.add(RideData("13July25","Nzb", "Hyd","10Am","Siddhipet"))
-        itemList.add(RideData(currentDate,"Nirmal", "Hyd","1Pm","Armoor"))
-        itemList.add(RideData("14July25","Hyd", "Adilabad","6Pm","Kamareddy"))
-        itemList.add(RideData("16July25","NZB", "Adilabad","6Pm","Kamareddy"))
-        itemList.add(RideData("16July25","NZB", "Adilabad","6Pm","Kamareddy"))
-        itemList.add(RideData("16July25","NZB", "Adilabad","6Pm","Kamareddy"))
-        itemList.add(RideData("20July25","JBS", "Adilabad","6Pm","Kamareddy"))
-
-
-        //Reading items from json file
-
-            val jsonString: String
-            try {
-                jsonString = applicationContext.assets.open("rideinputdata.json")
-                    .bufferedReader().use { it.readText() }
-            } catch (ioException: IOException) {
-                ioException.printStackTrace()
-                return
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> {
+                    replaceFragment(Home())
+                    true
+                }
+                R.id.profile -> {
+                    replaceFragment(Profile())
+                    true
+                }
+                R.id.settings -> {
+                    replaceFragment(Settings())
+                    true
+                }
+                else -> false
             }
-
-            val gson = Gson()
-            val listType = object : TypeToken<List<RideData>>() {}.type
-            val itemList2: List<RideData> = gson.fromJson(jsonString, listType)
-
-            // Now 'itemList' contains your parsed data, ready to be used
-            // e.g., for populating a RecyclerView
+        }
 
 
 
-        adapter1 = MyCustomAdapter(this, itemList2 as MutableList<RideData>)
-        listView.adapter = adapter1
+//
+//        //enableEdgeToEdge()
+//        //setContentView(R.layout.activity_main)
+//
+//
+//        val listView: ListView = findViewById(R.id.main_list)
+//        //listView = findViewById(R.id.main_list) // Assuming you have a ListView in activity_main.xml
+//
+//        // Date Format
+//        val sdf = SimpleDateFormat("dd/M/yyyy")
+//        val currentDate = sdf.format(Date())
+//        System.out.println(" Todays DATE is  "+currentDate)
+//
+//        //time Format
+//
+//        // Initialize with some data
+//        itemList.add(RideData( currentDate,"Hyd", "Armor","5Pm","Kamareddy"))
+//        itemList.add(RideData("13July25","Nzb", "Hyd","10Am","Siddhipet"))
+//        itemList.add(RideData(currentDate,"Nirmal", "Hyd","1Pm","Armoor"))
+//        itemList.add(RideData("14July25","Hyd", "Adilabad","6Pm","Kamareddy"))
+//        itemList.add(RideData("16July25","NZB", "Adilabad","6Pm","Kamareddy"))
+//        itemList.add(RideData("16July25","NZB", "Adilabad","6Pm","Kamareddy"))
+//        itemList.add(RideData("16July25","NZB", "Adilabad","6Pm","Kamareddy"))
+//        itemList.add(RideData("20July25","JBS", "Adilabad","6Pm","Kamareddy"))
+//
+//
+//        //Reading items from json file
+//
+//            val jsonString: String
+//            try {
+//                jsonString = applicationContext.assets.open("rideinputdata.json")
+//                    .bufferedReader().use { it.readText() }
+//            } catch (ioException: IOException) {
+//                ioException.printStackTrace()
+//                return
+//            }
+//
+//            val gson = Gson()
+//            val listType = object : TypeToken<List<RideData>>() {}.type
+//            val itemList2: List<RideData> = gson.fromJson(jsonString, listType)
+//
+//            // Now 'itemList' contains your parsed data, ready to be used
+//            // e.g., for populating a RecyclerView
+//
+//
+//
+//        adapter1 = MyCustomAdapter(this, itemList2 as MutableList<RideData>)
+//        listView.adapter = adapter1
+    }
+
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 
     fun Onclick_Search(view: View) {
